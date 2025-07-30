a65aad8599fd47c6822553a6f83e4f73680cd9bb
pvaz <pvaz@pvkss.homeland.local>
2025-07-30 19:36:24 -0300

    correccion dockerfile

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";  // Tu App.tsx que contiene BrowserRouter y las rutas
-import "./index.css";    // Tailwind u otros estilos globales
+import App from "./App";
+import "./index.css";
 
-// Busca el elemento root que debe estar en index.html
+
 const rootElement = document.getElementById("root") as HTMLElement;
 
 if (!rootElement) {
