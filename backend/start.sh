a2ea6e1e36c771969b16f016e983febbbca1a272
P4bl0V4z <pablovaz@dynamicdata.uy>
2025-10-07 14:33:34 -0300

    actualiza db prod1

--- backend/start.sh
+++ backend/start.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Running prisma migrate deploy..."
 # Aplica migraciones ya existentes a la DB real de producción
-pnpm prisma migrate deploy
+#pnpm prisma migrate deploy
 
 echo "Starting app..."
 node dist/main.js
